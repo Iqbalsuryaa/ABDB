0bba0d0fad3e9728fcfe751328582f5eef34d57c
Mohammad Iqbal Surya Ramadhan <112038977+Iqbalsuryaa@users.noreply.github.com>
2024-12-17 06:42:19 +0700

    Update ABDB_CurahHujanPendukungSektorPertanian.py

--- ABDB_CurahHujanPendukungSektorPertanian.py
+++ ABDB_CurahHujanPendukungSektorPertanian.py
@@ -282,10 +282,9 @@ if st.button("Klasifikasikan Cuaca"):
 st.write("\n---\n**Informasi Model**")
 st.write(f"Lokasi model: {MODEL_PATH}")
 st.write(f"Lokasi data: {DATA_PATH}")
-
 st.write("\n---\nDikembangkan oleh [M. Hasan basri]")
 
-    elif menu == "Clustering Dengan Metode K-Means":
+elif menu == "Clustering Dengan Metode K-Means":
     # kode untuk clustering K-Means
     st.title("Clustering Curah Hujan dengan Metode K-Means")
     st.write("Halaman ini akan berisi implementasi clustering data curah hujan dengan K-Means.")
